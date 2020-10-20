11238eb997fcb751037d081638aee64f30adea2c
monikaghosh <60311154+monikaghosh@users.noreply.github.com>
2020-10-20 22:26:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+
+  var holder_options={
+    isStatic: true
+  }
+
+holder = Bodies.rectangle(200,50,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0,
+  frictionAir:0
+
+}
+
+ball  = Bodies.circle(10,350,40,ball_options);
+World.add(world,ball);
+console.log(ball);
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.5,
+  length : 200
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+
+fill("White");
+}
+
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+
+  text("Press space bar to oscillate the pendulam to left and right with mouse",10,20);
+  text("Press Enter to stop the Pendulum from oscillating",100,35);
+
+fill ("green");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y,200,20);
+
+
+fill("pink");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(4);
+stroke("white");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y)
+
+if (keyDown("space")){
+  ball.position.x= mouseX;
+  ball.position.y = mouseY;
+  ball.isStatic = false;
+}
+if (keyDown("enter")){
+  
+  ball.isStatic = true;
+}
+
+drawSprites();
+}
+
+
+
