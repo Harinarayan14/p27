a970538ca0ae775fe6d56645f1d7d2fcc373d7e5
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2020-10-21 13:03:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,84 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
 
-var engine, world;  
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball;
+
 function setup() {
-  createCanvas(1200,600);
+  createCanvas(1000,500);
   engine = Engine.create();
   world = engine.world;
-  var option1={
-  isStatic:true
-  };
-  var option2={
-    frictionAir:0.01,
-    density :1,
-    restitution:1
-    };
-  
-  bob = Bodies.circle(200,50,30,option2);
-  fixedRect = Bodies.rectangle(600,50,200,30,option1);
-  chain = new Chain(bob,fixedRect);
-  World.add(world,bob);
-  World.add(world,fixedRect);
+
+
+  var holder_options={
+    isStatic: true
+  }
+
+holder = Bodies.rectangle(500,50,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0,
+  frictionAir:0
+
 }
+
+ball  = Bodies.circle(200,350,40,ball_options);
+World.add(world,ball);
+console.log(ball);
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.5,
+  length : 200
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+
+}
+
 
 function draw() {
-  background(0);   
-  fill("yellow");
-  textSize(25);
-  text("Press space key and mouse to oscillate the pendulum.",200,500);
-  text("Press Enter to set the pendulum.",250,450);
-  display1();
-  display2();
-  chain.display();
-  drawSprites();
-  if (keyCode === 32){
-    bob.position.y = mouseY;
-    bob.position.x = mouseX;
-}
-   if (keyCode === ENTER){
-    bob.position.y = 50;
-}
-Engine.update(engine);
-}
+  background(0);  
+  Engine.update(engine);
+  
+fill("white");
+textSize(20)
+  text("Press space bar to oscillate the pendulum .",10,300);
+  text("Press Enter to stop the Pendulum from oscillating",10,350);
+
+fill ("red");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y,200,20);
 
 
+fill("yellow");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(4);
+stroke("blue");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y)
+
+if (keyDown("space")){
+  ball.position.x= mouseX;
+  ball.position.y = mouseY;
+  ball.isStatic = false;
+}
+if (keyDown("enter")){
+  
+  ball.isStatic = true;
+}
+
+drawSprites();
+}
 
 
 
